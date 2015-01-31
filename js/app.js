4496cb66284ded9e000547d434da693fd0fe0bc5
zachbradshaw <bradshaw.zachary@gmail.com>
2015-01-31 14:59:53 -0500

    returning to basic functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var firstNameInput = document.querySelector('.first-name');
 var lastNameInput = document.querySelector('.last-name');
 var emailInput = document.querySelector('.email-input');
-var userIndex = document.querySelector('.user-index__list');
+var userIndex = document.querySelector('.user-index');
+var userList = document.querySelector('.user-index__list');
+var userListItem = document.querySelector('user-index__item');
 var userStore = ObjectStore();
 var form = document.querySelector('.user-login__info');
 var userArray = userStore.query();
@@ -17,16 +19,26 @@ var deleteUser = document.querySelector('.remove-user');
 form.addEventListener('submit', function(e) {
   e.stopPropagation();
   e.preventDefault();
+  // Un-comment this function if other method won't work.
   addUser();
+  // showUser();
   firstClear.value = "";
   lastClear.value = "";
   emailClear.value = "";
 });
 
-deleteUser.addEventListener('click', function(e) {
-  removeUser();
-});
+userList.addEventListener('click', function(e) {
+  if (e.target.nodeName === 'itemX') {
+    removeUser();
+  }
+})
 
+function removeUser() {
+  userStore.remove();
+}
+
+// This function works, but I'm trying other things.
+//
 function addUser() {
   var newUser = {
     firstName: firstNameInput.value,
@@ -42,58 +54,73 @@ function addUser() {
     var listDiv1 = document.createElement('div');
     var listDiv2 = document.createElement('div');
     var listDiv3 = document.createElement('div');
-    // var listEdit = document.createElement('div');
     var listClose = document.createElement('div');
 
     var itemFirst = document.createTextNode(newUser.firstName);
     var itemLast = document.createTextNode(newUser.lastName);
     var itemEmail = document.createTextNode(newUser.email);
-    // var itemEdit = document.createTextNode('j');
     var itemX = document.createTextNode('k');
 
-    userIndex.appendChild(listLi);
+    userList.appendChild(listLi);
 
     listLi.appendChild(listDiv1);
     listLi.appendChild(listDiv2);
     listLi.appendChild(listDiv3);
-    // listLi.appendChild(listEdit);
     listLi.appendChild(listClose);
 
     listDiv1.appendChild(itemFirst);
     listDiv2.appendChild(itemLast);
     listDiv3.appendChild(itemEmail);
-    // listEdit.appendChild(itemEdit);
     listClose.appendChild(itemX);
 
     listLi.className += "user-index__item";
     listDiv1.className += "user-name";
     listDiv2.className += "user-name";
     listDiv3.className += "user-email";
-    // listEdit.className += "edit-user";
     listClose.className += "remove-user";
   }
 }
 
-function searchUser () {
-  var searchInput = userSearch.value
+// function showUser() {
+//   var userListArray = userStore.query();
+//
+//   var listContainer = userIndex;
+//
+//   document.getElementsByClassName(".user-index")[0].appendChild(listContainer);
+//
+//   var listElement = userList;
+//
+//   listContainer.appendChild(listElement);
+//
+//   var numberOfUsers = userListArray.length;
+//
+//   for (var i = 0; i < numberOfUsers; ++i) {
+//
+//     var listItem = userListItem;
+//
+//     listItem.innerHMTL = userListArray[i];
+//
+//     listElement.appendChild(listItem);
+//
+//   }
+// }
 
-  if (userSearch === userArray.some()) {
-    alert('fire');
-  }
-}
+// function searchUser () {
+//   var searchInput = userSearch.value
+//
+//   if (userSearch === userArray.some()) {
+//     alert('fire');
+//   }
+// }
 
-function sortUser() {
-  userArray.sort(function(a, b){
-    var nameA = a.name.toLowerCase();
-    var nameB = b.name.toLowerCase();
-    if (nameA < nameB)
-      return -1
-    if (nameA > nameB)
-      return 1
-    return 0;
-  });
-}
-
-function removeUser() {
-  li.classList.remove("user-index__item");
-}
+// function sortUser() {
+//   userArray.sort(function(a, b){
+//     var nameA = a.name.toLowerCase();
+//     var nameB = b.name.toLowerCase();
+//     if (nameA < nameB)
+//       return -1
+//     if (nameA > nameB)
+//       return 1
+//     return 0;
+//   });
+// }
--- js/showUser.js
+++ js/showUser.js
@@ -1,7 +1,7 @@
 function showUsers(userArray) {
   var string = "";
   for (var i = 0; i < userArray.length; ++i) {
-    string += "<li class=list-item>" + userStore.query + "</li>";
+    string += "<li class=list-item>" + userArray + "</li>";
   }
 
 
